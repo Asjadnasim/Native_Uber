d6dabac325898d543bf3cf46eec75b6445f57fa7
Asjad Nasim <asjadnasim@gmail.com>
2024-08-18 20:22:21 +0530

    File & Folder Setup

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Onboarding = () => {
+	return (
+		<SafeAreaView className='flex h-full items-center justify-between bg-white'>
+			<TouchableOpacity>
+				<Text>Skip</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
+	);
+};
+
+export default Onboarding;
